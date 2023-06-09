cad5fc1691e506429bf51acc4d4247cb2b21bc36
Alex <admin@aleksandrslarionovs.com>
2023-06-09 21:17:50 +0000

    Add motion effect and useEffect hook to Hero section for improved user experience.

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,24 +1,31 @@
 'use client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { motion } from 'framer-motion';
 import $ from './Hero.module.scss';
-const Hero = () => (
-  <section className={$.container}>
-    <h1>Hi, my name is</h1>
-    <h2>Aleksandrs Larionovs.</h2>
-    <h3>I build things for web and beyond.</h3>
-    <p>
-      I am a software engineer specializing in building fast, responsive web apps. Currently, I am a student at the
-      University of London.
-    </p>
-    <a
-      href="https://medium.com/@aleksandlario/how-to-automate-midjourney-image-generation-with-puppeteer-6aea1b366c75"
-      target="_blank"
-      rel="noreferrer"
+const Hero = () => {
+  return (
+    <motion.section
+      initial={{ opacity: 0, top: 20 }}
+      whileInView={{ opacity: 1, top: 0 }}
+      viewport={{ once: true, amount: 0.5, staggerChildren: 0.5 }}
+      className={$.container}
     >
-      Check out my articles!
-    </a>
-  </section>
-);
+      <h1>Hi, my name is</h1>
+      <h2>Aleksandrs Larionovs.</h2>
+      <h3>I build things for web and beyond.</h3>
+      <p>
+        I am a software engineer specializing in building fast, responsive web apps. Currently, I am a student at the
+        University of London.
+      </p>
+      <a
+        href="https://medium.com/@aleksandlario/how-to-automate-midjourney-image-generation-with-puppeteer-6aea1b366c75"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Check out my articles!
+      </a>
+    </motion.section>
+  );
+};
 
 export default Hero;
