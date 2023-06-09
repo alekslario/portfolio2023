c95f36285e2f8ae6e00961de32a92dc42acabaa0
Alex <admin@aleksandrslarionovs.com>
2023-06-09 13:17:55 +0000

    "Update Articles component with recent articles and external link icon"

--- src/components/sections/Articles.js
+++ src/components/sections/Articles.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import $ from './Articles.module.scss';
+import { IconExternalLink } from '@tabler/icons-react';
 import Image from 'next/image';
 import config from '../../config';
 const Articles = () => {
   return (
     <section id="articles" className={$.container}>
-      <h2 className="numbered-heading">Articles</h2>
+      <h2 className="numbered-heading">Recent Articles</h2>
       <ul>
-        {config.articles.map(({ title, description, cover, link }, i) => (
-          <li key={i}></li>
+        {config.articles.map(({ title, description, cover, external }, i) => (
+          <li key={i}>
+            <h3>{title}</h3>
+            <p>{description}</p>
+            <a href={external} aria-label="External Link" className="external">
+              <IconExternalLink />
+            </a>
+          </li>
         ))}
       </ul>
     </section>
