e12f866a76dae3d78f3a5c07c12c74b04666e9a7
Alex <admin@aleksandrslarionovs.com>
2023-06-09 19:17:50 +0000

    Refactor animation and viewport settings for featured projects list.

--- src/components/sections/Featured.js
+++ src/components/sections/Featured.js
@@ -13,9 +13,9 @@ const Featured = () => (
       {config.featured.map(({ external, title, tech, github, cover, cta, description }, i) => {
         return (
           <motion.li
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            viewport={{ once: true }}
+            initial={{ opacity: 0, top: 120 }}
+            whileInView={{ opacity: 1, top: 0 }}
+            viewport={{ once: true, amount: 0.2 }}
             key={i}
             className={$.project}
           >
