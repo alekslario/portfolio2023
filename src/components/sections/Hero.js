df9d72d33950d42dcb0a909aa01a4cad8257ed3b
Alex <admin@aleksandrslarionovs.com>
2023-06-09 22:17:51 +0000

    "Added framer-motion animations to Hero section for improved user experience"

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -3,27 +3,38 @@ import React, { useEffect } from 'react';
 import { motion } from 'framer-motion';
 import $ from './Hero.module.scss';
 const Hero = () => {
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const listItem = {
+    hidden: { opacity: 0, transform: 'translateY(20px)' },
+    show: { opacity: 1, transform: 'translateY(0px)' },
+  };
+
   return (
-    <motion.section
-      initial={{ opacity: 0, top: 20 }}
-      whileInView={{ opacity: 1, top: 0 }}
-      viewport={{ once: true, amount: 0.5, staggerChildren: 0.5 }}
-      className={$.container}
-    >
-      <h1>Hi, my name is</h1>
-      <h2>Aleksandrs Larionovs.</h2>
-      <h3>I build things for web and beyond.</h3>
-      <p>
+    <motion.section variants={container} initial="hidden" animate="show" className={$.container}>
+      <motion.h1 variants={listItem}>Hi, my name is</motion.h1>
+      <motion.h2 variants={listItem}>Aleksandrs Larionovs.</motion.h2>
+      <motion.h3 variants={listItem}>I build things for web and beyond.</motion.h3>
+      <motion.p variants={listItem}>
         I am a software engineer specializing in building fast, responsive web apps. Currently, I am a student at the
         University of London.
-      </p>
-      <a
+      </motion.p>
+      <motion.a
+        variants={listItem}
         href="https://medium.com/@aleksandlario/how-to-automate-midjourney-image-generation-with-puppeteer-6aea1b366c75"
         target="_blank"
         rel="noreferrer"
       >
         Check out my articles!
-      </a>
+      </motion.a>
     </motion.section>
   );
 };
