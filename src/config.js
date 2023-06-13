695b74069b6441954824ec915c26c2e8ab261a98
Alex <admin@aleksandrslarionovs.com>
2023-06-13 17:47:45 +0000

    f

--- src/config.js
+++ src/config.js
@@ -50,7 +50,7 @@ const Config = {
       title: 'Remake of a Shopify theme.',
       description:
         '<p>This is a fully functional Shopify theme remake built with React and NodeJS backend that supports auth, email handling, stock control.</p> Contentful conveniently provides static content, enabling easy edits for non-coders. My aim was to ensure that the checkout process, in terms of appearance and flow, matches that of a standard Shopify store.',
-      cover: '/../public/images/palo-alto-redux.png',
+      cover: 'images/palo-alto-redux.png',
       tech: [
         'JS',
         'NextJS',
@@ -72,7 +72,7 @@ const Config = {
       title: 'Yet another algorithm visualizer.',
       description:
         'This is my take on algorithm visualization. No animation libraries were used, just pure CSS and abuse of pseudo-elements.',
-      cover: '/../public/images/algofind.png',
+      cover: 'images/algofind.png',
       tech: ['React', 'Tailwind', 'Styled Components'],
       github: 'https://github.com/alekslario/algofind',
       external: 'https://reliable-smakager-49c4c6.netlify.app/',
@@ -116,14 +116,14 @@ const Config = {
     {
       title: 'The Art of AI: Using ChatGPT and Midjourney to Generate Inspiring Visuals.',
       description: 'Exploring the beauty of randomness.',
-      cover: '/../public/images/1article.png',
+      cover: 'images/1article.png',
       external:
         'https://medium.com/@aleksandlario/the-art-of-ai-using-chatgpt-and-midjourney-to-generate-inspiring-visuals-caa91012c8db',
     },
     {
       title: 'How to Automate Midjourney Image Generation with Puppeteer.',
       description: 'Web scraping Discord.',
-      cover: '/../public/images/2article.png',
+      cover: 'images/2article.png',
       external: 'https://medium.com/@aleksandlario/how-to-automate-midjourney-image-generation-with-puppeteer-6aea1b366c75',
     },
   ],
